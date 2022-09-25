326b7bec6ce33533249d1c81b1ca599ffe681c32
TheGamer3514 <thegamer3514@gmail.com>
2022-09-25 08:32:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,8 +16,8 @@ EarlyUserRole: "", //Role Id For Early User
 VeryEarlyUserRole: "", //Role Id For Very Early User
 RegisteredRole: "", // Role Id For Registered Role
 
-GetStartedChannel: "", //Get Started Channel Id
-AutoRolesChannel: "", //Auto Roles Channel Id
-JoinLogsChannel: "",  //Join Logs Channel Id
-PublicLogsChannel: "" //Public Logs Channel Id
+GetStartedChannel: "", //Get Started Channel Id.
+AutoRolesChannel: "", //Auto Roles Channel Id. Auto Roles Go Here, Where users can get and remove roles.
+JoinLogsChannel: "",  //Join Logs Channel Id. Join Logs Go Here
+PublicLogsChannel: "" //Public Logs Channel Id. Some Logs Go Here, Does Not Have To Be Public
 }
