116f00952dde0159dceb05c9a2222cc9dca523d4
TheGamer3514 <gamer@3514.ga>
2022-10-13 10:27:10 +0100

    Update afk.js

--- src/commands/admin/afk.js
+++ src/commands/admin/afk.js
@@ -11,13 +11,13 @@ $setGlobalUserVar[AFKReason;$message]
 $argsCheck[>0;]
 
 $onlyIf[$getGlobalUserVar[AFK]!=Yes;You are already afk, to change your afk reason run **$getVar[Prefix]remove-afk** and run again this command.]
-$onlyForIDs[763471049894527006;752699129683247154;688693850180812856;967904098047381587;$getVar[OwnerID];You are not staff.]`
+$onlyForIDs[763471049894527006;752699129683247154;688693850180812856;967904098047381587;$getVar[OwnerID];You are required to be staff to use this command.]`
 }, {
 name: "$alwaysExecute",
 code: `
 $description[1;**__$username[$mentioned[1;no]]__ is afk for this reason:** $getGlobalUserVar[AFKReason;$mentioned[1;no]]
 
-> Please do not ping him and be patient.]
+> Please do not ping them and be patient.]
 $color[1;RED]
 $onlyIf[$getGlobalUserVar[AFK;$mentioned[1;no]]!=No]
 `
@@ -30,6 +30,6 @@ $setGlobalUserVar[AFK;No]
 $setGlobalUserVar[AFKReason;None.]
 
 $onlyIf[$getGlobalUserVar[AFK]!=No;You are not afk.]
-$onlyForIDs[763471049894527006;752699129683247154;688693850180812856;967904098047381587;$getVar[OwnerID];You are not staff.]
+$onlyForIDs[763471049894527006;752699129683247154;688693850180812856;967904098047381587;$getVar[OwnerID];You are required to be staff to use this command.]
 `
 }]
