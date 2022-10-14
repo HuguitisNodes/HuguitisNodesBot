fb9765b275488711a7dedd25ea2fe9d1e8e429ba
TheGamer3514 <gamer@3514.ga>
2022-10-14 19:35:17 +0100

    Update reboot.js

--- src/commands/admin/reboot.js
+++ src/commands/admin/reboot.js
@@ -4,6 +4,6 @@ aliases: "restart",
 code: `
 $reboot[Index.js]
 $sendMessage[**⚠️ Warning ⚠️**:**\`\`\`
-Manual Reboot has started by $userTag[$authorID]!! Now i will stop to respond to all commands for 5seconds - 1min min!\`\`\`**]
+Manual Reboot has started by $userTag[$authorID]!! Now i will stop to respond to all commands for 5seconds - 1min!\`\`\`**]
 $onlyForIDs[$getVar[OwnerID];** Only $username[$getVar[OwnerID]] can perform this action!!**]`
 });
