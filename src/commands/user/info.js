5ff6e665a25edc7e8f7ae055530857debb39bab8
TheGamer3514 <gamer@3514.ga>
2022-10-16 19:24:16 +0100

    Neater Credits

--- src/commands/user/info.js
+++ src/commands/user/info.js
@@ -4,18 +4,22 @@ module.exports = ({
     code: `
     $color[1;BLUE]
     $description[1;
-    **__Official Server Invite: __**
+    **__This Server Invite: __**
     > **$getVar[OfficialServerInvite]**
 
     **__Bot Config: __**
     > **Owner Id: $getVar[OwnerID]**
+    > **Staff Ids: $getVar[StaffIDs]**
     > **Prefix: $getVar[Prefix]**
     > **Success Emoji: $getVar[SuccessEmoji]**
     > **Error Emoji: $getVar[ErrorEmoji]**
     > **Loading Emoji: $getVar[LoadingEmoji]**
+    > **Spanish Role: $getVar[SpanishRole]**
+    > **English Role: $getVar[EnglishRole]**
+    
     **__Bot Credits:__**
-    - **Bot Repository:** https://github.com/HuguitisNodes/HuguitisNodesBot
-    - **Support Server:** https://discord.gg/CVbPZRt9yG
-    - **Website:** https://huguitisnodes.host
+    - [Bot Repository](https://github.com/HuguitisNodes/HuguitisNodesBot)
+    - [Support Server](https://discord.gg/CVbPZRt9yG)
+    - [Website](https://huguitisnodes.host)
     ]`
     })
