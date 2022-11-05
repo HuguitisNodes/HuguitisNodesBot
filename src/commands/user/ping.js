dae7f7c637da591b3b23b0e78667d598ce1163d6
TheGamer3514 <gamer@3514.ga>
2022-11-05 12:19:05 +0000

    Not copied! shhhhhh

--- src/commands/user/ping.js
+++ src/commands/user/ping.js
@@ -1,5 +1,7 @@
-module.exports = ({
 name: "ping",
-code: `Pinging...
-$editIn[1s;🏓**Pong!** $pingms]`
-});
+code: `
+$color[$random[0;99999]]
+$description[My ping is __**$ping MS**__]
+$footer[Requested by $username]
+$image[https://falsiskremlin.sirv.com/resim_2020-11-28_113400.png?text.0.text=$ping%20ms&text.0.position.x=-10%25&text.0.position.y=-25%25&text.0.size=60&text.0.color=ffffff&text.0.font.family=Play&watermark.0.image=%2FImages%2F784413323910709288.png&watermark.0.position.x=-35%25&watermark.0.scale.width=200&watermark.0.scale.height=200]`
+})
