e38a12fc3930b206e7f6dad445f76df1fcd9f7e4
Huguitis <mail@huguitisnodes.host>
2022-12-21 22:11:53 +0100

    Full rewrite

--- src/commands/user/leaderboard.js
+++ src/commands/user/leaderboard.js
@@ -1,9 +1,7 @@
 module.exports = [{
 name: "leaderboard",
 code: `
-$editMessage[$get[id];{newEmbed:{description:**__Amount of times mentioning $username[$getVar[OwnerID]]__:**
-$globalUserLeaderboard[HuguitisPinged;asc;**{top}: {username}** - {value} times.]
-
+$editMessage[$get[id];{newEmbed:{description:
 **__Amount of messages sent__:**
 $globalUserLeaderboard[MessagesSent;asc;**{top}: {username}** - {value} messages.]
 
