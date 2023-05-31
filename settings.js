62fc49b545d42f54c27f71cd405279b96f788c6d
Huguitis <mail@huguitisnodes.host>
2023-05-31 18:15:01 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,18 @@
 module.exports = {
 Token: "",
+PterodactylURL: "",
+PterodactylApiKey: "",
 ControlPanelURL: "",
 ControlPanelApiKey: "",
 IPQualityScoreApiKey: "",
-Prefix: "!",
-StaffIDs: "759796588842450944, 763471049894527006...",
+Prefix: "h!",
+StaffIDs: "759796588842450944, 861016878914601000, 688693850180812856...",
 LogsChannelID: "",
-OfficialServerInvite: "https://discord.gg/byarHVDznN",
+OfficialServerInvite: "https://discord.gg/freebothosting",
 
-SuccessEmoji: "",
-ErrorEmoji: "",
-LoadingEmoji: "",
+SuccessEmoji: "<a:success:978743201672347728>",
+ErrorEmoji: "<a:error:978743233779748894>",
+LoadingEmoji: "<a:loading:978743261374079027>",
 
 NewsPingRoleID: "",
 ChangelogsPingRoleID: "",
@@ -23,4 +25,5 @@ CoolGuyRoleID: "",
 GetStartedChannelID: "",
 AutoRolesChannelID: "",
 JoinLogsChannelID: "",
+FeedbackChannelID: ""
 }
